ad6f2be1ea61747f5830de816d6084f4a3c259e2
WesternGamer <80211714+WesternGamer@users.noreply.github.com>
2022-02-24 21:20:06 -0500

    Fixed wrong namespace.

--- ClientPlugin/Patches/SimulationUpdater.cs
+++ ClientPlugin/Patches/SimulationUpdater.cs
@@ -3,7 +3,7 @@ using VRage.Game;
 using VRage.Game.Components;
 #pragma warning disable CS0649
 
-namespace SpaceEngineersVR.Patches
+namespace ClientPlugin.Patches
 {
     [MySessionComponentDescriptor(MyUpdateOrder.BeforeSimulation)]
     class SimulationUpdater : MySessionComponentBase
