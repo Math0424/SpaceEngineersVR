4f3645122e8866a4a5eb7212f8eb40d0597e987a
Math0424 <Calculus0424@gmail.com>
2022-02-26 14:10:07 -0600

    Added model patcher (currently a dummy class)

---
+++ SpaceEngineersVR/Patches/ModelPatcher.cs
@@ -0,0 +1,24 @@
+﻿using Sandbox.Definitions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientPlugin.Patches
+{
+    internal static class ModelPatcher
+    {   
+
+        //TODO: call this method and swap the models for blocks, characters, and import other clientside models
+        //this class might get a bit messy as it needs to be called each time world loads too
+        public static void Patch()
+        {
+            //MyDefinitionManager.Static
+
+
+
+        }
+
+    }
+}
