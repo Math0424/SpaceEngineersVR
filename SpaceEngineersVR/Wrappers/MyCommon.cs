f48c6fc5ef5f1381e2b66562b6d68114d17f2ac0
Math0424 <Calculus0424@gmail.com>
2022-05-04 18:29:36 -0500

    Added MyCommon patch

---
+++ SpaceEngineersVR/Wrappers/MyCommon.cs
@@ -0,0 +1,29 @@
+﻿using HarmonyLib;
+using SharpDX.DXGI;
+using System;
+using System.Reflection;
+using VRageMath;
+using VRageRender;
+using VRageRender.Messages;
+
+namespace SpaceEngineersVR.Wrappers
+{
+    public static class MyCommon
+    {
+
+        static MyCommon()
+        {
+            Type t = AccessTools.TypeByName("VRageRender.MyCommon");
+
+            UpdateTimersDel = (Action)Delegate.CreateDelegate(typeof(Action), AccessTools.Method(t, "UpdateTimers"));
+        }
+
+        private static readonly Action UpdateTimersDel;
+        public static void UpdateTimers()
+        {
+            UpdateTimersDel.Invoke();
+        }
+
+    }
+
+}
